9c7a6b7e4921fbd46ae1f1c568019bf985439973
Robert de Gruijl <robertdegruijl@gmail.com>
2025-05-02 19:44:30 -0700

    update

--- code.py
+++ code.py
@@ -7,6 +7,9 @@ import adafruit_imageload
 import terminalio
 import pwmio
 import board
+import keypad
+import random
+
 
 from adafruit_display_text.label import Label
 from adafruit_display_shapes.circle import Circle
@@ -14,21 +17,12 @@ from adafruit_display_shapes.triangle import Triangle
 from adafruit_display_shapes.line import Line
 from adafruit_bitmap_font import bitmap_font
 
+keys = keypad.Keys((board.GP28,board.GP20, board.GP21, board.GP18, board.GP19), value_when_pressed=False, pull=False)
+
 # Hardware
 # Buzzer
 pwm = pwmio.PWMOut(board.GP22, variable_frequency=True)
 
-# Green buttons
-green_up = digitalio.DigitalInOut(board.GP18)
-green_up.direction = digitalio.Direction.INPUT
-green_down = digitalio.DigitalInOut(board.GP19)
-green_down.direction = digitalio.Direction.INPUT
-
-# Blue buttons
-blue_up = digitalio.DigitalInOut(board.GP20)
-blue_up.direction = digitalio.Direction.INPUT
-blue_down = digitalio.DigitalInOut(board.GP21)
-blue_down.direction = digitalio.Direction.INPUT
 
 # rgb matrix LED display
 displayio.release_displays()
@@ -41,13 +35,18 @@ display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
 
 g = displayio.Group()       # main screen
 border = displayio.Group()
-splash = displayio.Group()  # splash screen
 
 # Sounds
 def edgebeep():
     pwm.frequency = 1300
     pwm.duty_cycle = 0x7fff
     time.sleep(.01)
+    pwm.duty_cycle = 0
+
+def clickbeep():
+    pwm.frequency = 1500
+    pwm.duty_cycle = 0x7fff
+    time.sleep(.001)
     pwm.duty_cycle = 0
 
 def paddlebeep():
@@ -114,13 +113,14 @@ score_font = bitmap_font.load_font("fonts/4x6.bdf")
 win_font = bitmap_font.load_font("fonts/VictoriaMedium-8.bdf")
 
 # bitmap for images
+s_palette = displayio.Palette(4)
+s_palette[0] = 0x000000 # red
+s_palette[1] = 0xFF0000 # green
+s_palette[2] = 0x00FF00 # blue
+s_palette[3] = 0x0000FF # blue
 bitmap = displayio.Bitmap(64, 32, 4)
-tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=s_palette)
 g.append(tile_grid)
-
-
-
-
 
 display.root_group = g
 target_fps = 1
@@ -145,19 +145,120 @@ def paddlebal_win():
     g.remove(win_screen)
     display.refresh(minimum_frames_per_second=0)
 
+def game_over():
+    # Popup screen for game win
+    text = "Game Over..."
+    win_screen = Label(text=text, font=terminalio.FONT, color=palette[15], line_spacing=.7, scale=1)
+    win_screen.x = 10
+    win_screen.y = -5
+    g.append(win_screen)
+    for i in range(20):
+        win_screen.y += 1
+        display.refresh(minimum_frames_per_second=0)
+    for i in range(3):
+        display.refresh(minimum_frames_per_second=0)
+        goalbeep(2)
+        win_screen.text = ""
+        display.refresh(minimum_frames_per_second=0)
+        goalbeep(2)
+        win_screen.text = text
+    g.remove(win_screen)
+    display.refresh(minimum_frames_per_second=0)
+
+
 # Startup/Welcome screen
 def welcome_screen():
+    splash = displayio.Group()  # splash screen
     display.root_group = splash
     for c in range(63):
         w = Line(c, 0, c, 32, palette[c])
         splash.append(w)
         display.refresh(minimum_frames_per_second=0)
+    time.sleep(2)
     for c in range(63):
         splash.pop()
         time.sleep(0.01)
         display.refresh(minimum_frames_per_second=0)
+    del splash
     display.root_group = g
 
+# splash 1
+def splash_0():
+    splash = displayio.Group()  # splash screen
+    display.root_group = splash
+    for c in range(31, 0, -1):
+        w0 = Line(0, 0, 0, 31, palette[c+20])
+        w1 = Line(63, 0, 63, 31, palette[c+20])
+        splash.append(w0)
+        splash.append(w1)
+        while w0.x < c:
+            w0.x = w0.x + 1
+            w1.x = w1.x - 1
+            display.refresh(minimum_frames_per_second=0)
+    del splash
+    display.refresh(minimum_frames_per_second=0)
+    display.root_group = g
+
+def snake():
+    snake_head = (7,5)
+    snake_body = [snake_head]
+    snake_head = (snake_head[0] + 1, snake_head[1])
+    snake_body.append(snake_head)
+    for q in snake_body:
+        draw_pixel(bitmap, q[0], q[1], 1)
+    direction = 'right'
+    fruit = (random.randint(0, 63), random.randint(0, 31))
+    # Game scores
+    score = 0
+    score1_label = Label(text=str(score), font=score_font, color=palette[39], line_spacing=.7, scale=1)
+    score1_label.x = 0
+    score1_label.y = 5
+    g.append(score1_label)
+    level = 0.2
+    play = True
+    while play:
+        event = keys.events.get()
+        if event:
+            if event.pressed:
+                clickbeep()
+                if event.key_number == 0:
+                    play = False
+                if event.key_number == 1:
+                    direction = 'down'
+                if event.key_number == 2:
+                    direction = 'up'
+                if event.key_number == 3:
+                    direction = 'right'
+                if event.key_number == 4:
+                    direction = 'left'
+        if direction == 'right':
+            snake_head = (snake_head[0] + 1, snake_head[1]) 
+        if direction == 'left':
+            snake_head = (snake_head[0] - 1,  snake_head[1])
+        if direction == 'down':
+            snake_head = (snake_head[0], snake_head[1] + 1) 
+        if direction == 'up':
+            snake_head = (snake_head[0], snake_head[1] - 1) 
+        if snake_head[0] > 63 or snake_head[1] > 31 or snake_head[0] < 0 or snake_head[1] < 0 or snake_head in snake_body:
+            game_over()
+            play = False
+            bitmap.fill(0)
+            g.remove(score1_label)
+            continue
+        snake_body.append(snake_head)
+        draw_pixel(bitmap, snake_head[0], snake_head[1], 1)
+        draw_pixel(bitmap, fruit[0], fruit[1], 2)
+        if snake_head == fruit:
+            goalbeep(1)
+            fruit = (random.randint(0, 63), random.randint(0, 31))
+            level = level - 0.01
+            score = score+1
+            score1_label.text = str(score)
+        else:
+            snake_tail = snake_body.pop(0)
+            draw_pixel(bitmap, snake_tail[0], snake_tail[1], 0)
+        display.refresh(minimum_frames_per_second=0)
+        time.sleep(level)
 
 def pong():
     speed = 0.04
@@ -194,6 +295,10 @@ def pong():
     g.append(bob1)
     g.append(bob2)
 
+    key1pressed = False
+    key2pressed = False
+    key3pressed = False
+    key4pressed = False
 
     while playpong:
         play = True
@@ -203,21 +308,47 @@ def pong():
             ball.x = ball.x + ball_x_direction
             bally = bally + ball_y_direction
             ball.y = round(bally)
+            print(playpong)
+            event = keys.events.get()
+            if event:
+                if event.key_number == 0:
+                    if event.pressed:
+                        playpong = False
+                        continue
+                if event.key_number == 1:
+                    if event.pressed:
+                        key1pressed = True
+                    else:
+                        key1pressed = False
+                if event.key_number == 2:
+                    if event.pressed:
+                        key2pressed = True
+                    else:
+                        key2pressed = False
+                if event.key_number == 3:
+                    if event.pressed:
+                        key3pressed = True
+                    else:
+                        key3pressed = False
+                if event.key_number == 4:
+                    if event.pressed:
+                        key4pressed = True
+                    else:
+                        key4pressed = False
 
-            # Read green buttons
-            if green_down.value == 0:
+            if key1pressed:
                 if bob1.y > 0:
                     bob1.y = bob1.y - 1
-            if green_up.value == 0:
+            if key2pressed:
                 if bob1.y < 31 - paddle_width:
                     bob1.y = bob1.y + 1
-            # Read blue buttons
-            if blue_down.value == 0:
+            if key3pressed:
                 if bob2.y > 0:
                     bob2.y = bob2.y - 1
-            if blue_up.value == 0:
+            if key4pressed:
                 if bob2.y < 31 - paddle_width:
                     bob2.y = bob2.y + 1
+
 
             # Bounce on paddle
             if ball.x == 63 - ball_radius * 2:
@@ -253,6 +384,12 @@ def pong():
             if ball.y == 31 - 2 * ball_radius:
                 edgebeep()
                 ball_y_direction = ball_y_direction * - 1
+    g.remove(score1_label)
+    g.remove(score2_label)
+    g.remove(bob1)
+    g.remove(bob2)
+    g.remove(ball)
+
 
 def scroll_image():
     b, p = adafruit_imageload.load("images/dog1.bmp")
@@ -263,6 +400,16 @@ def scroll_image():
         t.x = x
         time.sleep(.001)
         display.refresh(minimum_frames_per_second=0)
+    time.sleep(2)
+    for y in range(0, -30, -1):
+        t.y = y
+        time.sleep(.001)
+        display.refresh(minimum_frames_per_second=0)
+    g.remove(t)
+
+    
+
+def scroll_text():
     text1="Ellie's room, stay cool!! ..."
     scrolling_label1 = Label(text=text1, font=terminalio.FONT, color=palette[58], line_spacing=.7, scale=1)
     text2="No little Brothers allowed!"
@@ -287,15 +434,20 @@ def scroll_image():
     g.remove(scrolling_label1)
     g.remove(scrolling_label2)
     g.remove(scrolling_label3)
-    g.remove(t)
 
 def lightshow():
     doshow = True
-    while doshow:    
+    while doshow:
+        event = keys.events.get()
+        if event:
+            if event.key_number == 0:
+                if event.pressed:
+                    doshow = False
+                    continue
         welcome_screen()
         scroll_image()
-        if not blue_down.value:
-            doshow = False
+        splash_0()
+        scroll_text()
         
     
 
@@ -305,14 +457,14 @@ mode = 'show'
 
 
 def select_mode():
-    modes = {'pong': None, 'show': None}
+    modes = {'snake': None, 'pong': None, 'show': None}
     top_line = 8
     top_arrow = 7
     line_spacing = 8
     select_values = []
 
-    border_width = 3
-    border_color = palette[32]
+    border_width = 2
+    border_color = palette[51]
     for c in range(border_width):
         w = Line(c, 0, c, 32, border_color)
         border.append(w)
@@ -339,30 +491,30 @@ def select_mode():
     g.append(arrow)
     select = True
     while select:
-        if not green_down.value:
-            arrow.y = arrow.y + line_spacing
-            display.refresh(minimum_frames_per_second=0)
-            edgebeep()
-            while not green_down.value:
-                time.sleep(.001)
-        if not green_up.value:
-            arrow.y = arrow.y - line_spacing
-            display.refresh(minimum_frames_per_second=0)
-            edgebeep()
-            while not green_up.value:
-                time.sleep(.001)
+        event = keys.events.get()
+        if event:
+            if event.pressed:
+                if event.key_number == 0:
+                    selectbeep(1)
+                    q = int(arrow.y/line_spacing)
+                    g.remove(border)
+                    g.remove(arrow)
+                    for m in modes.keys():
+                        g.remove(modes[m])
+                    return select_values[q]
+                if event.key_number == 1:
+                    arrow.y = arrow.y + line_spacing
+                    display.refresh(minimum_frames_per_second=0)
+                    edgebeep()
+                if event.key_number == 2:
+                    arrow.y = arrow.y - line_spacing
+                    display.refresh(minimum_frames_per_second=0)
+                    edgebeep()
+
         if arrow.y > top_arrow+line_spacing*(len(modes)-1):
             arrow.y = top_arrow+line_spacing*(len(modes)-1)
         if arrow.y < top_arrow:
             arrow.y = top_arrow
-        if not blue_up.value:
-            selectbeep(1)
-            q = int(arrow.y/line_spacing)
-            g.remove(border)
-            g.remove(arrow)
-            for m in modes.keys():
-                g.remove(modes[m])
-            return select_values[q]
         display.refresh(minimum_frames_per_second=0)
  
 
@@ -373,3 +525,5 @@ while True:
         pong()
     if mode == 'show':
         lightshow()
+    if mode == 'snake':
+        snake()
